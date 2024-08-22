076922396a5a81bfdbcff0ce9e11e8b07d8005ea
marcus pedersen <marcus@mpedersen.me>
2024-08-22 09:41:03 +0200

    Add gitignore again

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -22,6 +22,20 @@ namespace api.Controllers
         public IActionResult GetAll()
         {
             var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
         }
     }
 }
